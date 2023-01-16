abda21fd58b5ff419a7cde8352aec326bc64eb1d
omnomrose <maryrosenguyenbusiness@gmail.com>
2023-01-15 21:27:19 -0800

    stylizing

--- pages/counter.js
+++ pages/counter.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/router'
+import styles from '../styles/counter.module.css'
 
 export default function Counter (){
     const [number, setNumber] = useState (0);
@@ -14,12 +15,28 @@ export default function Counter (){
 
     return (
         <>
-        <div>COUNTER PAGE</div>
-        <div> Welcome {isUser}</div>
+        <main className={styles.main}>
+        <div className= {styles.header}>
 
-        <div>{number}</div>
-        <button onClick ={() => setNumber (number + 1)}>Increment</button>
-        <button onClick ={() => setNumber (number - 1)}>Decrement</button>
+        <div className= {styles.counter}>COUNTER PAGE</div>
+        <div className= {styles.welcome}> Welcome, {isUser}.</div>
+
+        </div>
+
+        <div className={styles.btnnumbers}>
+
+        <div className={styles.numbers}>{number}</div>
+        <div className={styles.buttons}>
+        <button className={styles.increment} onClick ={() => setNumber (number + 1)}>Increment</button>
+        <button className={styles.decrement} onClick ={() => setNumber (number - 1)}>Decrement</button>
+        </div>
+
+        </div>
+        
+      
+        
+        </main>
         </>
+
     )
 }
